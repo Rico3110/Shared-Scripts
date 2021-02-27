5e9e09627f9b94904df9d6a842225e79e43ea7a7
lasseho <54005894+lasseho@users.noreply.github.com>
2021-02-27 22:00:51 +0100

    market

---
+++ Structures/Buildings/Market.cs
@@ -0,0 +1,64 @@
+﻿using Shared.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.HexGrid;
+using UnityEngine;
+
+namespace Shared.Structures
+{
+    class Market : RefineryBuilding
+    {
+        public override byte MaxLevel => 3;
+        public override byte[] MaxHealths => new byte[]{
+            50
+        };
+
+        public override int[] RessourceLimits => new int[] {
+            6,
+            12,
+            26
+        };
+
+        public override int MaxProgress => 10;
+        public override Dictionary<RessourceType, int> InputRecipe => new Dictionary<RessourceType, int> { { RessourceType.COAL, 1 }, { RessourceType.STONE, 1 } };
+        public override Dictionary<RessourceType, int> OutputRecipe => new Dictionary<RessourceType, int> { { RessourceType.IRON, 1 } };
+
+
+        public override Dictionary<RessourceType, int>[] Recipes
+        {
+            get
+            {
+                Dictionary<RessourceType, int>[] result = {
+                    new Dictionary<RessourceType, int>{ { RessourceType.WOOD, 3 }, { RessourceType.STONE, 2 }  }
+                };
+                return result;
+            }
+        }
+
+        public Smelter() : base()
+        {
+            
+        }
+
+        public Smelter(
+            HexCell Cell,
+            byte Tribe,
+            byte Level,
+            byte Health,
+            TroopInventory TroopInventory,
+            BuildingInventory Inventory,
+            int Progress
+            ) : base(Cell, Tribe, Level, Health, TroopInventory, Inventory, Progress)
+        {
+            
+        }
+
+        public override bool IsPlaceable(HexCell cell)
+        {            
+            return base.IsPlaceable(cell) ;
+        }
+    }
+}
